a2925cc2f40d22c876bf6dffce2f63a1ac6a0b04
Yuvraj Chandra <Singhyuvraj179@gmail.com>
2022-07-22 11:37:22 +0530

    Extract text from PDF

--- main.py
+++ main.py
@@ -9,3 +9,9 @@ pdfReader = PyPDF3.PdfFileReader(book)
 
 pages = pdfReader.numPages
 
+finalText = ""
+with pdfplumber.open(file) as pdf:
+    for i in range(0, pages): 
+        page = pdf.pages[i]
+        text = page.extract_text()
+        finalText += text
